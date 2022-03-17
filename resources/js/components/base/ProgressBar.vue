a23103fca6f585d89196e9cb765fca5ac924965c
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2022-03-17 13:27:29 -0400

    Change stripes to ProgressBar.vue

--- resources/js/components/base/ProgressBar.vue
+++ resources/js/components/base/ProgressBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative pt-2 max-w-full">
+  <div class="relative pt-2 max-w-full mb-3">
     <div class="flex mb-2 items-center justify-between space-x-6">
       <div>
         <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-indigo-600 bg-indigo-200">
@@ -12,10 +12,8 @@
         </span>
       </div>
     </div>
-    <div class="overflow-hidden h-3 mb-4 text-xs flex rounded bg-indigo-200">
-      <div
-        class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-indigo-500 w-progress"
-      ></div>
+    <div class="flex h-4 overflow-hidden text-xs bg-slate-200 rounded">
+      <div class="flex flex-col justify-center overflow-hidden text-white whitespace-nowrap bg-blue-600 transition-[width] ease duration-75 bg-striped bg-[length:1rem_1rem] animate-progress-striped w-progress"></div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,30 +22,18 @@ module.exports = {
             opacity: 1,
           },
         },
-        'progress-stripes': {
-          '100%': {
-            backgroundPosition: '-100px 0px',
+        'progress-bar-striped': {
+          '0%': {
+            backgroundPositionX: '1rem',
           },
         },
       },
       animation: {
         'fade-in-down': 'fade-in-down 0.2s ease-in-out both',
-        'progress-stripes': 'animate-stripes 5s linear infinite',
-      },
-      boxShadow: {
-        'inner-progress': '0 2px 5px rgba(0, 0, 0, 0.25) inset',
-      },
-      backgroundSize: {
-        'progress': '35px 20px, 100% 100%, 100% 100%',
+        'progress-striped': 'progress-bar-striped 1s linear infinite',
       },
       backgroundImage: {
-        'progress': '-webkit-linear-gradient(-45deg, \n' +
-          '\t                           transparent 33%, rgba(0, 0, 0, .1) 33%, \n' +
-          '\t                           rgba(0,0, 0, .1) 66%, transparent 66%),\n' +
-          '\t   -webkit-linear-gradient(top, \n' +
-          '\t                           rgba(255, 255, 255, .25), \n' +
-          '\t                           rgba(0, 0, 0, .25)),\n' +
-          '\t   -webkit-linear-gradient(left, #09c, #f44)',
+        'striped': 'linear-gradient(45deg,rgba(255,255,255,.15) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.15) 50%,rgba(255,255,255,.15) 75%,transparent 75%,transparent)',
       }
     },
   },
