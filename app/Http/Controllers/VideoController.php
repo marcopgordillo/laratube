2ac2b21958b741acb6298969f43a4a6cf6f442f2
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2022-03-17 15:34:01 -0400

    Add ConvertForStreaming.php Job

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreVideoRequest;
 use App\Http\Resources\ChannelResource;
+use App\Jobs\Videos\ConvertForStreaming;
 use App\Models\Channel;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Validation\ValidationException;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 
 class VideoController extends Controller
 {
@@ -24,8 +28,9 @@ class VideoController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
+     * @throws ValidationException
      */
     public function store(StoreVideoRequest $request, Channel $channel): JsonResource
     {
@@ -33,9 +38,21 @@ class VideoController extends Controller
 
         if (isset($data['video'])) {
             $channel->clearMediaCollection('videos');
-            $channel->addMedia($data['video'])
+            try {
+                $media = $channel->addMedia($data['video'])
                     ->withCustomProperties(['title' => $data['title']])
                     ->toMediaCollection('videos');
+            } catch (FileDoesNotExist $e) {
+                throw ValidationException::withMessages([
+                    'video' => 'File does not exists.'
+                ]);
+            } catch (FileIsTooBig $e) {
+                throw ValidationException::withMessages([
+                    'video' => 'File is Too Big.'
+                ]);
+            }
+
+            $this->dispatch(new ConvertForStreaming($media));
         }
 
         $channel->append('videos');
